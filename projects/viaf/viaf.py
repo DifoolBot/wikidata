b395fb08d2b20a075bb5567dabf514e84222bf25
DifoolBot <remko@rechoka.com>
2023-11-17 13:53:52 +0700

    * class for AuthoritySource * code changed to class * generates duplicates report * reference based on on heuristic * viaf call to SourceID end-point; with justlinks.json

--- projects/viaf/viaf.py
+++ projects/viaf/viaf.py
@@ -1,9 +1,8 @@
 import requests
 import json
-from requests.utils import requote_uri
 import pywikibot as pwb
 from datetime import datetime
-import re
+import os.path
 
 
 WD = 'http://www.wikidata.org/entity/'
@@ -15,191 +14,266 @@ PID_UNION_LIST_OF_ARTIST_NAMES_ID = 'P245'
 PID_STATED_IN = 'P248'
 PID_RETRIEVED = 'P813'
 PID_REFERENCE_URL = 'P854'
+PID_BASED_ON_HEURISTIC = 'P887'
+
 QID_VIRTUAL_INTERNATIONAL_AUTHORITY_FILE = 'Q54919'
+QID_INFERRED_FROM_VIAF_ID_CONTAINING_AN_ID_ALREADY_PRESENT_IN_THE_ITEM = 'Q115111315'
+
+AUTHORITY_SOURCE_CODE_GETTY_RESEARCH_INSTITUTE = 'JPG'
+AUTHORITY_SOURCE_CODE_WIKIDATA = 'WKP'
 
 SITE = pwb.Site('wikidata', 'wikidata')
 SITE.login()
 SITE.get_tokens('csrf')
 REPO = SITE.data_repository()
 
+DUPLICATES_FILE = 'duplicates.json'
+PAGE_TITLE = 'User:Difool/viaf_already_somewhere'
+ 
 
-def query_wdqs(query):
-    data = requests.get(WDQS_ENDPOINT, params={'query': query, 'format': 'json'}).json()
-    return data['results']['bindings']
+class AuthoritySource:
+    pass
 
-
-def query_viaf(name):
-    query = 'local.personalNames all "{name}" and local.sources any "jpg"'.format(name = name)
-    data = requests.get(VIAF_ENDPOINT, params={'query': query, 'httpAccept': 'application/json', 'sortKeys': 'holdingscount'}).json()
-    return data['searchRetrieveResponse']['records']
-
-
-def check_viaf_record(record, getty_id, qid):
-    res = {}
-    record_data = record['record']['recordData']
-
-    res['viaf_id'] = record_data['viafID']
-    res['has_getty'] = False
-    res['has_wikidata'] = False
-
-    source = record_data['sources']['source']
-
-    if isinstance(source, dict):
-        source = [source]
-    for s in source:
-        id = s['@nsid']
-        text = s['#text']
-        if text.startswith('JPG|'):
-            if getty_id != id:
-                return None
-            else:
-                res['has_getty'] = True
-        elif text.startswith('WKP|'):
-            if qid != id:
-                return None
-            else:
-                res['has_wikidata'] = True
-    return res
-
+class GettyAuthoritySource(AuthoritySource):
+    def getCode(self):
+        return AUTHORITY_SOURCE_CODE_GETTY_RESEARCH_INSTITUTE
     
-def create_viaf_ref(viaf_id):
-    today = datetime.today() 
-    viaf_url = "https://viaf.org/viaf/{viaf}/".format(viaf = viaf_id)
+    def getPID(self):
+        return PID_UNION_LIST_OF_ARTIST_NAMES_ID
+    
+    def getDescription(self):
+        return 'Getty ULAN'
 
-    stated_in = pwb.Claim(REPO, PID_STATED_IN)  
-    stated_in.setTarget(pwb.ItemPage(REPO, QID_VIRTUAL_INTERNATIONAL_AUTHORITY_FILE))
-    id = pwb.Claim(REPO, PID_VIAF_ID)  
-    id.setTarget(viaf_id)
-    retr = pwb.Claim(REPO, PID_RETRIEVED) 
-    dateCre = pwb.WbTime(year=int(today.strftime("%Y")), month=int(today.strftime("%m")), day=int(today.strftime("%d")))
-    retr.setTarget(dateCre)  
-    ref = pwb.Claim(REPO, PID_REFERENCE_URL)  
-    ref.setTarget(viaf_url)
+class ViafBot:
 
-    return [stated_in, id, retr, ref]
+    def __init__(self, auth_src):
+        self.auth_src = auth_src
 
+    def run(self):
+        self.duplicates = self.load_duplicates()
+        self.iterate()
+        self.write_to_wiki(self.make_wikitext())
 
-def is_viaf_source(src, viaf_id) -> bool:
-    if PID_VIAF_ID in src:
-        for claim in src[PID_VIAF_ID]:
-            if (claim.getTarget() == viaf_id):
-                return True
-
-    regex = r"https?:\/\/viaf.org\/viaf\/{viaf_id}\/?".format(viaf_id=viaf_id)
-
-    if PID_REFERENCE_URL in src:
-        for claim in src[PID_REFERENCE_URL]:
-            url = claim.getTarget()
-            matches = re.search(regex, url, re.IGNORECASE)
-            if matches:
-                return True
-
-    return False
+    def query_wdqs(self, query):
+        data = requests.get(WDQS_ENDPOINT, params={'query': query, 'format': 'json'}).json()
+        return data['results']['bindings']
 
 
-def has_viaf_source(claim, viaf_id) -> bool:
-    srcs = claim.getSources() 
-    for src in srcs: 
-        if is_viaf_source(src, viaf_id):
-            return True
+    def query_viaf(self, local_auth_id):
+        try:
+            url = 'https://viaf.org/viaf/sourceID/{code}|{local_auth_id}/justlinks.json'.format(code=self.auth_src.getCode(), local_auth_id=local_auth_id)
+            data = requests.get(url).json()
+            return data
+        except:
+            print('{desc} ID {local_auth_id} returns an error'.format(desc=self.auth_src.getDescription(), local_auth_id=local_auth_id))
+            return []
 
-    return False
+    def check_viaf_justlinks(self, data, qid, local_auth_id):
+        res = {}
+
+        if 'viafID' not in data:
+            return None
+
+        viaf_id = data['viafID']
+        res['viaf_id'] = viaf_id
+        res['has_local_auth_id'] = False
+        res['has_wikidata'] = False
+        res['has_error'] = False
+
+        code = self.auth_src.getCode()
+
+        if code in data:
+            if local_auth_id in data[code]:
+                res['has_local_auth_id'] = True
+            elif len(data[code]) > 0:
+                print("viaf id {viaf_id} of {qid} has another {desc} link".format(viaf_id=viaf_id, qid=qid, desc=self.auth_src.getDescription()))
+                res['has_error'] = True
+
+            if len(data[code]) > 2:
+                print("viaf id {viaf_id} of {qid} has multiple {desc} links".format(viaf_id=viaf_id, qid=qid, desc=self.auth_src.getDescription()))
+                res['has_error'] = True
+                    
+        if AUTHORITY_SOURCE_CODE_WIKIDATA in data:
+            if qid in data[AUTHORITY_SOURCE_CODE_WIKIDATA]:
+                res['has_wikidata'] = True
+            elif len(data[AUTHORITY_SOURCE_CODE_WIKIDATA]) > 0:
+                print("viaf id {viaf_id} of {qid} has another wikidata link".format(viaf_id=viaf_id, qid=qid))
+                res['has_error'] = True
+
+            if len(data[AUTHORITY_SOURCE_CODE_WIKIDATA]) > 2:
+                print("viaf id {viaf_id} of {qid} has multiple wikidata links".format(viaf_id=viaf_id, qid=qid))
+                res['has_error'] = True
+            
+        return res
+
+        
+    def create_viaf_ref(self, viaf_id):
+        today = datetime.today() 
+
+        stated_in = pwb.Claim(REPO, PID_STATED_IN)  
+        stated_in.setTarget(pwb.ItemPage(REPO, QID_VIRTUAL_INTERNATIONAL_AUTHORITY_FILE))
+
+        id = pwb.Claim(REPO, PID_VIAF_ID)  
+        id.setTarget(viaf_id)
+
+        retr = pwb.Claim(REPO, PID_RETRIEVED) 
+        dateCre = pwb.WbTime(year=int(today.strftime("%Y")), month=int(today.strftime("%m")), day=int(today.strftime("%d")))
+        retr.setTarget(dateCre)  
+
+        ref = pwb.Claim(REPO, PID_BASED_ON_HEURISTIC)  
+        ref.setTarget(pwb.ItemPage(REPO, QID_INFERRED_FROM_VIAF_ID_CONTAINING_AN_ID_ALREADY_PRESENT_IN_THE_ITEM))
+
+        return [stated_in, id, retr, ref]
 
 
-def change_wikidata(qid, viaf_id, getty_id, has_wikidata) -> None:
-    if not qid.startswith('Q'):  # ignore property pages and lexeme pages
-        return
 
-    item = pwb.ItemPage(REPO, qid)
+    def change_wikidata(self, qid, viaf_id) -> None:
+        if not qid.startswith('Q'):  # ignore property pages and lexeme pages
+            return
 
-    if not item.exists():
-        return
+        item = pwb.ItemPage(REPO, qid)
 
-    if item.isRedirectPage():
-        return
+        if not item.exists():
+            return
 
-    existing_claims = item.get().get("claims")
+        if item.isRedirectPage():
+            return
 
-    if not item.botMayEdit():
-        print("Skipping %s because it cannot be edited by bots" % qid)
-        return
+        existing_claims = item.get().get("claims")
 
-    if PID_VIAF_ID in existing_claims:
-        print("Skipping %s because it already has a VIAF ID" % qid)
-        return
+        if not item.botMayEdit():
+            print("Skipping %s because it cannot be edited by bots" % qid)
+            return
 
-    print('Adding VIAF ID {viaf_id} to {qid}'.format(viaf_id=viaf_id, qid=qid))
-    try:
-        claim = pwb.Claim(REPO, PID_VIAF_ID)
-        claim.setTarget(viaf_id)
-        item.addClaim(claim, summary="Adding VIAF ID based on Union List of Artist Names ID")
+        if PID_VIAF_ID in existing_claims:
+            print("Skipping %s because it already has a VIAF ID" % qid)
+            return
 
-        if has_wikidata:
-            claim.addSources(create_viaf_ref(viaf_id), summary="Adding VIAF reference")
+        print('Adding VIAF ID {viaf_id} to {qid}'.format(viaf_id=viaf_id, qid=qid))
+        try:
+            claim = pwb.Claim(REPO, PID_VIAF_ID)
+            claim.setTarget(viaf_id)
+            item.addClaim(claim, summary="Adding VIAF ID based on {desc} ID".format(desc=self.auth_src.getDescription()))
 
-        if PID_UNION_LIST_OF_ARTIST_NAMES_ID in existing_claims:
-            for claim in existing_claims[PID_UNION_LIST_OF_ARTIST_NAMES_ID]:
-                if claim.getTarget() != getty_id:
-                    continue
-                
-                if has_viaf_source(claim, viaf_id):
-                    continue
+            claim.addSources(self.create_viaf_ref(viaf_id), summary="Adding VIAF reference")
 
-                claim.addSources(create_viaf_ref(viaf_id), summary="Adding VIAF reference")
-                break
+        except Exception as e:
+            print("Error adding claims: %s" % e)
 
-    except Exception as e:
-        print("Error adding claims: %s" % e)
+    def get_duplicates_qids(self, viaf_id):
+        res = []
+        query = 'SELECT DISTINCT ?item WHERE {{ ?item p:P214 ?statement0. ?statement0 (ps:P214) "{viaf_id}".}} LIMIT 5'.format(viaf_id=viaf_id)
+        
+        for row in self.query_wdqs(query):
+            qid = row.get('item', {}).get('value', '').replace(WD, '')
+            res.append(qid)
+        return res
 
 
-def iterate_viaf(qid, name, getty_id) -> None:
-    for record in query_viaf(name):
-        res = check_viaf_record(record, getty_id, qid)
-        if res is not None:
-            viaf_id = res['viaf_id']
-            has_getty = res['has_getty']
-            has_wikidata = res['has_wikidata']
-            if has_getty:
-                change_wikidata(qid, viaf_id, getty_id, has_wikidata)
+    def iterate_viaf(self, qid, local_auth_id) -> None:
+        if qid in self.duplicates:
+            return 
 
+        res = self.check_viaf_justlinks(self.query_viaf(local_auth_id), qid, local_auth_id)
+        if res is None:
+            return
+        
+        viaf_id = res['viaf_id']
+        has_local_auth_id = res['has_local_auth_id']
+        has_error = res['has_error']
+
+        if not has_local_auth_id:
+            print('{qid} has no {desc} link'.format(qid=qid, desc=self.auth_src.getDescription()))
+            return
+
+        duplicate_qids = self.get_duplicates_qids(viaf_id)
+        if duplicate_qids != []:
+            print('{qid} has duplicates: {dupl}'.format(qid=qid, dupl=duplicate_qids))
+            for duplicate_qid in duplicate_qids:
+                if qid not in self.duplicates:
+                    self.duplicates[qid] = []
+                self.duplicates[qid].append(
+                    {'duplicate_qid': duplicate_qid, 'auth_code': self.auth_src.getCode(), 'local_auth_id': local_auth_id, 'viaf_id': viaf_id}
+                     )
+            self.save_duplicates(self.duplicates)
+        elif has_error:
+            print('{qid} has errors, but no duplicates'.format(qid=qid))
+        else:
+            print('{qid} -> {viaf_id}; {desc} {local_auth_id}'.format(qid=qid, viaf_id=viaf_id, desc=self.auth_src.getDescription(), local_auth_id=local_auth_id))
+            self.change_wikidata(qid, viaf_id)
+            
+
+    def make_wikitext(self):
+        header = '{| class="wikitable" style="vertical-align:bottom;"\n|-\n! VIAF\n! QID on the item\n! ID from cluster\n! 2nd QID'
+        body = ''
+        line = '\n|-\n| https://viaf.org/viaf/{viaf_id}\n| {{{{Q|{qid}}}}}\n| {auth_code}|{local_auth_id}\n| {{{{Q|{duplicate_qid}}}}}'
+        for qid in self.duplicates:
+            for row in self.duplicates[qid]:
+                duplicate_qid = row['duplicate_qid']
+                auth_code = row['auth_code']
+                local_auth_id = row['local_auth_id']
+                viaf_id = row['viaf_id']
+                body = body + line.format(viaf_id=viaf_id, qid=qid, auth_code=auth_code, local_auth_id=local_auth_id, duplicate_qid=duplicate_qid)
+        footer = '\n|}'
+
+        wikitext = f'{header}{body}{footer}'
+
+        return wikitext
+    
+    def write_to_wiki(self, wikitext) -> None:
+        #print(wikitext)
+        site = pwb.Site('wikidata', 'wikidata')
+        page = pwb.Page(site, PAGE_TITLE)
+        page.text = wikitext
+        page.save(summary='upd', minor=False)
+
+    def iterate(self):
+        # instance of (P31)
+        # VIAF ID (P214)
+        # Union List of Artist Names ID (P245)
+
+        # humans with a Union List of Artist Names ID - not deprecated, without a VIAF ID
+        query_template = """SELECT DISTINCT ?item ?local_auth_id WHERE {{
+                                    ?item p:{pid} ?statement0.
+                                    ?statement0 ps:{pid} _:anyValueP245;
+                                        wikibase:rank ?rank.
+                                    ?item p:P31 ?statement1.
+                                    ?statement1 ps:P31 wd:Q5.
+                                    FILTER(?rank != wikibase:DeprecatedRank)
+                                    ?item wdt:{pid} ?local_auth_id.
+                                    MINUS {{
+                                        ?item p:P214 ?statement2.
+                                        ?statement2 ps:P214 _:anyValueP214.
+                                    }}
+                                    }}
+                                    """
+
+        qry = query_template.format(pid=self.auth_src.getPID())
+        for row in self.query_wdqs(qry):
+            qid = row.get('item', {}).get('value', '').replace(WD, '')
+            local_auth_id = row.get('local_auth_id', {}).get('value', '')
+            if len(qid) == 0:
+                continue
+            if len(local_auth_id) == 0:
+                continue
+            self.iterate_viaf(qid, local_auth_id)
+
+    def load_duplicates(self):
+        if os.path.exists(DUPLICATES_FILE):
+            with open(DUPLICATES_FILE, "r") as infile:
+                duplicates = json.load(infile)
+        else:
+            duplicates = {}
+        return duplicates
+
+    def save_duplicates(self, duplicates):
+        with open(DUPLICATES_FILE, "w") as outfile:
+            json.dump(duplicates, outfile)
 
 def main() -> None:
-    # instance of (P31)
-    # VIAF ID (P214)
-    # Union List of Artist Names ID (P245)
 
-    # humans with a Union List of Artist Names ID - not deprecated, without a VIAF ID
-    query_template = """SELECT DISTINCT ?item ?itemLabel ?Union_List_of_Artist_Names_ID WHERE {
-                            SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-                            {
-                                SELECT DISTINCT ?item ?Union_List_of_Artist_Names_ID WHERE {
-                                ?item p:P245 ?statement0.
-                                ?statement0 ps:P245 _:anyValueP245;
-                                    wikibase:rank ?rank.
-                                ?item p:P31 ?statement1.
-                                ?statement1 ps:P31 wd:Q5.
-                                FILTER(?rank != wikibase:DeprecatedRank)
-                                ?item wdt:P245 ?Union_List_of_Artist_Names_ID.
-                                MINUS {
-                                    ?item p:P214 ?statement2.
-                                    ?statement2 ps:P214 _:anyValueP214.
-                                }
-                                }
-                            }
-                        }"""
-
-    for row in query_wdqs(query_template):
-        qid = row.get('item', {}).get('value', '').replace(WD, '')
-        name = row.get('itemLabel', {}).get('value', '')
-        getty_id = row.get('Union_List_of_Artist_Names_ID', {}).get('value', '')
-        if len(qid) == 0:
-            continue
-        if len(name) == 0:
-            continue
-        if len(getty_id) == 0:
-            continue
-        iterate_viaf(qid, name, getty_id)
-
+    bot = ViafBot(GettyAuthoritySource())
+    bot.run()
 
 if __name__=='__main__':
     main()
