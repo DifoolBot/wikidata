54331c7a2d60fc2b84e39bd931f6ba7e6aaaebde
DifoolBot <remko@rechoka.com>
2026-07-01 22:23:48 +0700

    Add .gitattributes to enforce LF line endings for shell scripts

--- projects/remove_import_ref/toolforge_run.sh
+++ projects/remove_import_ref/toolforge_run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Toolforge launcher for remove_import_ref.
 #
+# Invoke via `bash` so the file's executable bit is irrelevant (it is often
+# lost when committed from Windows):
+#
 # Dry-run:
 #   toolforge jobs run rmimport --image python3.11 --wait \
-#       --command "$HOME/wikidata/projects/remove_import_ref/toolforge_run.sh"
+#       --command "bash $HOME/wikidata/projects/remove_import_ref/toolforge_run.sh"
 # Real edit (append --save):
-#       --command "$HOME/wikidata/projects/remove_import_ref/toolforge_run.sh --save"
+#       --command "bash $HOME/wikidata/projects/remove_import_ref/toolforge_run.sh --save"
 #
 # Assumes: repo cloned at $HOME/wikidata, venv at $HOME/venv, pywikibot config
 # in $HOME/.pywikibot. Override with VENV / PYWIKIBOT_DIR env vars.
